ec24289958e96366506e7b8a7c2f92179e7a416b
jjb-str <jjb@steere.com>
2023-08-02 10:39:58 -0400

    Finish calc logic

--- script.js
+++ script.js
@@ -25,27 +25,68 @@ function operate (a, b, operand) {
 
 function updateDisplay(text) {
     display = document.querySelector('.display');
-    if (parseInt(display.textContent) > 99999999) return;
+    if (parseInt(display.textContent) > 9999999) return;
     oldText = display.textContent;
     display.textContent = oldText + text;
 }
 
 function clearDisplay() {
     display = document.querySelector('.display');
-    display.textContent = ''
+    display.textContent = '';
+}
+
+function asignNumber(number) {
+    if (numberA === undefined) {
+        numberA = number;
+    } else if (numberB === undefined) {
+        numberB = number
+    } else {
+        numberB = undefined;
+    }
 }
 
 let numberA;
 let numberB;
-let operarion;
+let numberC;
+let operation;
 
 numbers = document.querySelectorAll('.number');
 numbers.forEach((element) => {
-    element.addEventListener('click', function (element) {updateDisplay(element.target.textContent)})    
+    element.addEventListener('click', function (element) {updateDisplay(element.target.textContent);});    
 });
 
-clearBtn = document.querySelector('.clr-btn')
-clearBtn.addEventListener('click', function () {clearDisplay()})
+operands = document.querySelectorAll('.operand');
+operands.forEach((element) => {
+    element.addEventListener('click', function (element) {
+        switch(element.target.textContent) {
+            case '+' : operation = '+'; break;
+            case '-' : operation = '-'; break;
+            case '*' : operation = '*'; break;
+            case '/' : operation = '/'; break;
+        };
+        
+        number = parseInt(document.querySelector('.display').textContent);
+        asignNumber(number);
+        clearDisplay();
+    });
+});
+
+
+clearBtn = document.querySelector('.clr-btn');
+clearBtn.addEventListener('click', function () {clearDisplay(); numberA = undefined; numberB = undefined; numberC = undefined; operation = undefined; console.log(numberA, numberB, operation)});
 
 compute = document.querySelector('.compute');
-compute.addEventListener('click', function () {operate(numberA, numberB, operation)})
+compute.addEventListener('click', function () { 
+    if (numberB === undefined) {
+        numberB = parseInt(document.querySelector('.display').textContent)
+    }
+    if (numberC === undefined) {
+        numberC = (operate(numberA, numberB, operation));
+        console.log(numberA + operation + numberB + '=' + numberC)
+    } else {
+        numberA = numberC
+        numberC = (operate(numberA, numberB, operation));
+        console.log(numberA + operation + numberB + '=' + numberC);
+    }; 
+    document.querySelector('.display').textContent = numberC;
+});
